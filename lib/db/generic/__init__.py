23f4724c82bc785f9ac770db3dc74842e4a85469
Sanjarbek Hudaiberdiev <hudaiber@lmem04.be-md.ncbi.nlm.nih.gov>
2016-06-21 11:14:22 -0400

    major update 2.

--- lib/db/generic/__init__.py
+++ lib/db/generic/__init__.py
@@ -20,17 +20,40 @@ def neighborhoods_path():
         print _path
         raise IOError("The neighborhoods path doesn't exist. Check the project data paths.")
 
+def map_file2org():
 
-def map_profile2id(dataset):
+    _fname = os.path.join(gv.project_data_path, 'cas1402/file2org.txt')
+    out_map = {}
+    for l in open(_fname).readlines():
+        terms = l.strip().split()
+        out_map[terms[0]] = terms[1]
+
+    return out_map
+
+
+def map_profile2id(prefix):
     _db = DbClass()
-    _db.cmd = """select code, id from cdd_profiles_clusters where dataset in (0, %d)""" % dataset
+    _db.cmd = """select code, id from %s_profiles""" % prefix
     rows = _db.retrieve()
     return {row[0]: int(row[1]) for row in rows}
 
 
-def map_profiles_id2code_code2def():
+def map_file_id2name(prefix):
     _db = DbClass()
-    _db.cmd = """select id, code, description from cdd_profiles_clusters"""
+    _db.cmd = """select id, name from %s_files""" % prefix
+    return {str(l[0]): l[1] for l in _db.retrieve()}
+
+
+def map_file_name2id(prefix):
+    _db = DbClass()
+    _db.cmd = """ select name, id from  %s_files""" % prefix
+    rows = _db.retrieve()
+    return {row[0]: row[1] for row in rows}
+
+
+def map_profiles_id2code_code2def(prefix):
+    _db = DbClass()
+    _db.cmd = """select id, code, description from cdd_profiles"""
     rows = _db.retrieve()
 
     _id2code = dict()
@@ -42,16 +65,3 @@ def map_profiles_id2code_code2def():
         _code2def[_code] = _def
 
     return _id2code, _code2def
-
-
-def map_file_id2name():
-    _db = DbClass()
-    _db.cmd = """select id, name from crispr_wgs_files"""
-    return {str(l[0]): l[1] for l in _db.retrieve()}
-
-
-def map_file_name2id(dataset):
-    _db = DbClass()
-    _db.cmd = """ select name, id from  crispr_wgs_files where dataset=%d""" % dataset
-    rows = _db.retrieve()
-    return {row[0]: row[1] for row in rows}
